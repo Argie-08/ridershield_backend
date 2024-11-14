c54533122b698497e9c1f095a15c204616ff5f68
sparksoft-ssdd <ssdd@sparksoft.com.ph>
2024-11-14 22:14:27 +0800

    nginx again

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -10,7 +10,7 @@ class HandleCors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')  // Allows all origins (change '*' to your frontend URL for better security)
+            ->header('Access-Control-Allow-Origin', 'ridershield-backend.onrender.com')  // Allows all origins (change '*' to your frontend URL for better security)
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')  // Allowed methods
             ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization, Origin');  // Allowed headers
     }
